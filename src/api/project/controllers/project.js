8bce0ca2e39b94ea148cd6c781cd7494df0bbced
Ahmed Hossam <1318039@gmail.com>
2022-02-07 19:50:28 +0200

    Update permissions for project

--- src/api/project/controllers/project.js
+++ src/api/project/controllers/project.js
@@ -1,9 +1,47 @@
-'use strict';
+"use strict";
 
 /**
  *  project controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::project.project');
+module.exports = createCoreController("api::project.project", ({ strapi }) => ({
+  async create(ctx) {
+    ctx.request.body.data.owner = ctx.state.user;
+
+    return await super.create(ctx);
+  },
+
+  async update(ctx) {
+    const query = {
+      filters: {
+        id: ctx.params.id,
+        owner: { id: ctx.state.user.id },
+      },
+    };
+    const project = await super.find({ query: query });
+
+    if (!project.data || !project.data.length) {
+      return ctx.unauthorized("You can't update this entry");
+    }
+
+    return await super.update(ctx);
+  },
+
+  async delete(ctx) {
+    const query = {
+      filters: {
+        id: ctx.params.id,
+        owner: { id: ctx.state.user.id },
+      },
+    };
+    const project = await super.find({ query: query });
+
+    if (!project.data || !project.data.length) {
+      return ctx.unauthorized("You can't update this entry");
+    }
+
+    return await super.delete(ctx);
+  },
+}));
