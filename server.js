f1b52c56465ec86c4b768a32599e11999d92daec
Ahmed Hossam <1318039@gmail.com>
2022-02-22 12:01:07 +0200

    Change port

--- server.js
+++ server.js
@@ -199,6 +199,6 @@ server.use(/^(?!\/auth).*$/, (req, res, next) => {
 
 server.use(router);
 
-server.listen(8000, () => {
+server.listen(process.env.PORT || 8000, () => {
   console.log("Run Auth API Server");
 });
